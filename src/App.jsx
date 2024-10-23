20c1e9497e08cbe0d52869ba11fc8d116ec7c18d
Osama <abdulhaihafeez@gmail.com>
2024-10-23 13:07:10 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+function App() {
+  
+
+  return (
+    <>
+    <div class="intro">
+      <h1>Welcome to my website!</h1>
+    </div>
+    <p class="summary">
+      You can find my thoughts here.
+      <br></br>
+      <b>And</b> <i>pictures</i> of scientists!
+    </p>
+    </>
+  );
+}
+
+export default App
